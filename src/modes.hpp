24c0ff303a3f6309a818c230f438ebffffbb2b94
magistermaks <magistermaks@darktree.net>
2020-09-24 14:37:46 +0200

    Updated declarations

--- src/modes.hpp
+++ src/modes.hpp
@@ -12,16 +12,30 @@
 #include "lib/argparse.hpp"
 #include "lib/whereami.h"
 
+// linux only headers
+#include <unistd.h>
+#include <linux/limits.h>
+
 typedef int (*DynLibInit) (seq::Executor*,seq::FileHeader*);
 
 #define SEQ_LIB_NAME "native.so"
 
-void help( ArgParse& argp );
-void build( ArgParse& argp );
-void run( ArgParse& argp );
+// command line flags
+struct Options {
+	bool verbose: 1;
+	bool force_execution: 1;
+};
 
-// utils - implemented in ./utils.cpp
+void help( ArgParse& argp, Options opt );
+void build( ArgParse& argp, Options opt );
+void run( ArgParse& argp, Options opt );
+
+// utils - implemented in utils.cpp
 bool file_exist( const char *path );
 std::string get_exe_path();
+std::string get_cwd_path();
+std::string get_base_name( std::string path );
+size_t get_path_hash( std::string path );
+std::string get_absolute_path( std::string relative, std::string base );
 
 #endif /* MODES_HPP_ */
