9271b9a471bfc367ffa0b8043a97ae2701e41c44
magistermaks <magistermaks@darktree.net>
2020-09-26 15:07:01 +0200

    Better

--- src/std/common.hpp
+++ src/std/common.hpp
@@ -3,11 +3,14 @@
 #define STD_COMMON_HPP_
 
 #define SEQ_IMPLEMENT
+#define SEQ_EXCLUDE_COMPILER
 #include "../api/SeqAPI.hpp"
 
 #define INIT_SUCCESS 0
 #define INIT_ERROR 1
 #define INIT extern "C" int init
 #define EMPTY seq::Stream()
+
+#undef SEQ_EXCLUDE_COMPILER
 
 #endif /* STD_COMMON_HPP_ */
