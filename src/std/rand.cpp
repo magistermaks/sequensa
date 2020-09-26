424cf0a2889d32c451f72194db960d3e9b057ab5
magistermaks <magistermaks@darktree.net>
2020-09-26 17:10:08 +0200

    Added rand.cpp (stdlib)

---
+++ src/std/rand.cpp
@@ -0,0 +1,34 @@
+
+#include "common.hpp"
+
+seq::Stream seq_std_rand( seq::Stream input ) {
+	seq::Stream output;
+
+	for( int i = input.size(); i > 0; i -- ) {
+
+		output.push_back( seq::Generic( new seq::type::Number( false, rand() ) ) );
+
+	}
+
+	return output;
+}
+
+seq::Stream seq_std_srand( seq::Stream input ) {
+	seq::Stream output;
+
+	for( auto& arg : input ) {
+
+		srand( seq::util::numberCast( arg ).Number().getLong() );
+
+	}
+
+	return output;
+}
+
+INIT( seq::Executor* exe, seq::FileHeader* head ) {
+
+	exe->inject( "std:rand"_b, seq_std_rand );
+	exe->inject( "std:srand"_b, seq_std_srand );
+
+	return INIT_SUCCESS;
+}
