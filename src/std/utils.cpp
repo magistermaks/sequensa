5f5960f587a8fc945c067fea263bb36e15df40e5
magistermaks <magistermaks@darktree.net>
2020-10-08 17:45:17 +0200

    Added 'std:call' and 'std:sleep'

--- src/std/utils.cpp
+++ src/std/utils.cpp
@@ -1,5 +1,7 @@
 
 #include "common.hpp"
+#include <chrono>
+#include <thread>
 
 seq::Stream seq_std_call( seq::Stream& input ) {
 	seq::Stream output;
@@ -17,9 +19,23 @@ seq::Stream seq_std_call( seq::Stream& input ) {
 	return output;
 }
 
+seq::Stream seq_std_sleep( seq::Stream& input ) {
+	seq::Stream output;
+
+	for( auto& arg : input ) {
+
+		std::chrono::milliseconds ms { seq::util::numberCast(arg).Number().getLong() };
+		std::this_thread::sleep_for( ms );
+
+	}
+
+	return output;
+}
+
 INIT( seq::Executor* exe, seq::FileHeader* head ) {
 
 	exe->inject( "std:call"_b, seq_std_call );
+	exe->inject( "std:sleep"_b, seq_std_sleep );
 
 	const seq::Generic value_true = seq::Generic( new seq::type::Bool( false, true ) );
 	const seq::Generic value_false = seq::Generic( new seq::type::Bool( false, false ) );
