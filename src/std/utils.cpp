58fd755d299deb50e652492e89a70d17544e46ea
magistermaks <magistermaks@darktree.net>
2020-10-08 17:17:19 +0200

    Added 'std:call'

--- src/std/utils.cpp
+++ src/std/utils.cpp
@@ -1,7 +1,25 @@
 
 #include "common.hpp"
 
+seq::Stream seq_std_call( seq::Stream& input ) {
+	seq::Stream output;
+
+	for( auto& arg : input ) {
+
+		long val = seq::util::numberCast(arg).Number().getLong();
+
+		while( val --> 0 ) {
+			output.push_back( seq::Generic( new seq::type::Null( false ) ) );
+		}
+
+	}
+
+	return output;
+}
+
 INIT( seq::Executor* exe, seq::FileHeader* head ) {
+
+	exe->inject( "std:call"_b, seq_std_call );
 
 	const seq::Generic value_true = seq::Generic( new seq::type::Bool( false, true ) );
 	const seq::Generic value_false = seq::Generic( new seq::type::Bool( false, false ) );
