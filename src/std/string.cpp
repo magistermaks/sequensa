352ae7e250054b73ccf344ca15ae0f34d3d0fd58
magistermaks <magistermaks@darktree.net>
2020-10-14 15:40:24 +0200

    Expanded std string

--- src/std/string.cpp
+++ src/std/string.cpp
@@ -68,12 +68,66 @@ seq::Stream seq_std_split( seq::Stream& input ) {
 	return output;
 }
 
+seq::Stream seq_std_explode( seq::Stream& input ) {
+
+	seq::Stream output;
+
+	for( long i = 0; i < (long) input.size(); i ++ ) {
+
+		seq::string str = seq::util::stringCast( input[i] ).String().getString();
+
+		for( long j = 0; j < (long) str.size(); j ++ ) {
+
+			seq::string chr = ""_b;
+			chr += (seq::byte) str[j];
+			output.push_back( seq::util::newString( chr.c_str() ) );
+
+		}
+
+	}
+
+	return output;
+}
+
+seq::Stream seq_std_from_code( seq::Stream& input ) {
+
+	seq::Stream output;
+
+	for( long i = 0; i < (long) input.size(); i ++ ) {
+
+		seq::byte code = (seq::byte) seq::util::numberCast( input[i] ).Number().getLong();
+		seq::string str = ""_b;
+		str += (seq::byte) code;
+		output.push_back( seq::util::newString( str.c_str() ) );
+
+	}
+
+	return output;
+}
+
+seq::Stream seq_std_to_code( seq::Stream& input ) {
+
+	seq::Stream output;
+
+	for( long i = 0; i < (long) input.size(); i ++ ) {
+
+		seq::byte code = seq::util::stringCast( input[i] ).String().getString()[0];
+		output.push_back( seq::util::newNumber( code ) );
+
+	}
+
+	return output;
+}
+
 INIT( seq::Executor* exe, seq::FileHeader* head ) {
 
 	exe->inject( "std:uppercase"_b, seq_std_uppercase );
 	exe->inject( "std:lowercase"_b, seq_std_lowercase );
 	exe->inject( "std:concat"_b, seq_std_concat );
 	exe->inject( "std:split"_b, seq_std_split );
+	exe->inject( "std:explode"_b, seq_std_explode );
+	exe->inject( "std:from_code"_b, seq_std_from_code );
+	exe->inject( "std:to_code"_b, seq_std_to_code );
 
 	return INIT_SUCCESS;
 }
