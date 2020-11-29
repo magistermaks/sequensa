411d34d89fa2760f038a3bd9240a1056df30a70d
magistermaks <magistermaks@darktree.net>
2020-11-29 20:19:57 +0100

    Added 'std:mixin' to std-lang

--- src/std/lang.cpp
+++ src/std/lang.cpp
@@ -26,6 +26,8 @@
 #define NO_EXCLUDE_COMPILER
 #include "common.hpp"
 
+seq::Executor* executor;
+
 seq::Stream seq_std_eval( seq::Stream& input ) {
 	seq::Stream output;
 
@@ -40,7 +42,32 @@ seq::Stream seq_std_eval( seq::Stream& input ) {
 			seq::Executor exe;
 			exe.execute( bb );
 
-			output.push_back( exe.getResult() );
+			output.insert(output.end(), exe.getResults().begin(), exe.getResults().end());
+
+		}catch( std::exception& error ) {
+			output.push_back( seq::util::newNull() );
+		}
+
+	}
+
+	return output;
+}
+
+seq::Stream seq_std_mixin( seq::Stream& input ) {
+	seq::Stream output;
+
+	for( auto& arg : input ) {
+
+		try{
+
+			seq::string code = seq::util::stringCast(arg).String().getString();
+			auto buf = seq::Compiler::compile( code );
+			seq::ByteBuffer bb( buf.data(), buf.size() );
+
+			seq::Executor exe(executor);
+			exe.execute( bb );
+
+			output.insert(output.end(), exe.getResults().begin(), exe.getResults().end());
 
 		}catch( std::exception& error ) {
 			output.push_back( seq::util::newNull() );
@@ -53,6 +80,9 @@ seq::Stream seq_std_eval( seq::Stream& input ) {
 
 INIT( seq::Executor* exe, seq::FileHeader* head ) {
 
+	executor = exe;
+
+	exe->inject( "std:mixin"_b, seq_std_mixin );
 	exe->inject( "std:eval"_b, seq_std_eval );
 
 	return INIT_SUCCESS;
